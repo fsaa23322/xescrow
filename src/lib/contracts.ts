b253c97fe7b7f7d8bb90f1c1c946710f68d0fc70
XEscrow Admin <admin@xescrow.io>
2026-01-14 13:03:29 +0000

    Fix: Restore missing ERC20_ABI

--- src/lib/contracts.ts
+++ src/lib/contracts.ts
@@ -1,5 +1,7 @@
+// 1. 你的主网合约地址
 export const ESCROW_CONTRACT_ADDRESS = "0xfFEA4d8EbE310F7bc1D70ee58b3D27cfd3B6D9e7";
 
+// 2. 核心业务 ABI
 export const ESCROW_ABI = [
   {
     "inputs": [{"internalType": "string","name": "_title","type": "string"},{"internalType": "uint256","name": "_amount","type": "uint256"},{"internalType": "address","name": "_seller","type": "address"},{"internalType": "uint256","name": "_sellerDeposit","type": "uint256"},{"internalType": "uint256","name": "_daysToDeliver","type": "uint256"}],
@@ -47,5 +49,30 @@ export const ESCROW_ABI = [
     ],
     "name": "ProjectCreated",
     "type": "event"
+  }
+] as const;
+
+// 3. 补回被删掉的 ERC20 ABI (USDT授权用)
+export const ERC20_ABI = [
+  {
+    "constant": true,
+    "inputs": [{"name": "_owner","type": "address"}],
+    "name": "balanceOf",
+    "outputs": [{"name": "balance","type": "uint256"}],
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [{"name": "_spender","type": "address"},{"name": "_value","type": "uint256"}],
+    "name": "approve",
+    "outputs": [{"name": "","type": "bool"}],
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [{"name": "_owner","type": "address"},{"name": "_spender","type": "address"}],
+    "name": "allowance",
+    "outputs": [{"name": "","type": "uint256"}],
+    "type": "function"
   }
 ] as const;
