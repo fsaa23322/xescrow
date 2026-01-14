e8e9f52613e2801f6d3966f8e0e62b010452a69e
XEscrow Admin <admin@xescrow.io>
2026-01-14 12:59:55 +0000

    Fix: Update to correct Mainnet Contract Address

--- src/lib/contracts.ts
+++ src/lib/contracts.ts
@@ -1,122 +1,51 @@
-import { parseAbi } from 'viem';
-
-// 1. 合约地址 (已更新为你的真实 BSC 部署地址)
 export const ESCROW_CONTRACT_ADDRESS = "0xfFEA4d8EbE310F7bc1D70ee58b3D27cfd3B6D9e7";
 
-// 2. USDT/Token ABI (用于授权 Approve)
-export const ERC20_ABI = parseAbi([
-  "function approve(address spender, uint256 amount) external returns (bool)",
-  "function allowance(address owner, address spender) view returns (uint256)",
-  "function balanceOf(address account) view returns (uint256)"
-]);
-
-// 3. XEscrow 核心 ABI
 export const ESCROW_ABI = [
-  // --- 读方法 (Read) ---
   {
-    "type": "function",
-    "name": "projects",
-    "inputs": [{"name": "id", "type": "uint256"}],
-    "outputs": [
-      {"name": "buyer", "type": "address"},
-      {"name": "seller", "type": "address"},
-      {"name": "tokenAddress", "type": "address"},
-      {"name": "amount", "type": "uint256"},
-      {"name": "sellerDeposit", "type": "uint256"},
-      {"name": "terms", "type": "string"},
-      {"name": "duration", "type": "uint256"},
-      {"name": "deadline", "type": "uint256"},
-      {"name": "state", "type": "uint8"},
-      {"name": "extensionProposedSeconds", "type": "uint256"},
-      {"name": "extensionProposer", "type": "address"},
-      {"name": "confirmTime", "type": "uint256"},
-      {"name": "workSubmittedTime", "type": "uint256"}
-    ],
-    "stateMutability": "view"
-  },
-  {
-    "type": "function",
-    "name": "projectCount",
-    "inputs": [],
-    "outputs": [{"name": "", "type": "uint256"}],
-    "stateMutability": "view"
-  },
-  {
-    "type": "function",
-    "name": "feePercent",
-    "inputs": [],
-    "outputs": [{"name": "", "type": "uint256"}],
-    "stateMutability": "view"
-  },
-
-  // --- 写方法 (Write) ---
-  {
-    "type": "function",
+    "inputs": [{"internalType": "string","name": "_title","type": "string"},{"internalType": "uint256","name": "_amount","type": "uint256"},{"internalType": "address","name": "_seller","type": "address"},{"internalType": "uint256","name": "_sellerDeposit","type": "uint256"},{"internalType": "uint256","name": "_daysToDeliver","type": "uint256"}],
     "name": "createProject",
-    "inputs": [
-      {"name": "_seller", "type": "address"},
-      {"name": "_terms", "type": "string"},
-      {"name": "_durationInHours", "type": "uint256"},
-      {"name": "_tokenAddress", "type": "address"},
-      {"name": "_amount", "type": "uint256"},
-      {"name": "_sellerDepositAmount", "type": "uint256"}
-    ],
-    "outputs": [],
-    "stateMutability": "nonpayable"
+    "outputs": [{"internalType": "uint256","name": "","type": "uint256"}],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    "type": "function",
+    "inputs": [{"internalType": "uint256","name": "_projectId","type": "uint256"}],
     "name": "confirmProject",
-    "inputs": [{"name": "_id", "type": "uint256"}],
     "outputs": [],
-    "stateMutability": "nonpayable"
+    "stateMutability": "payable",
+    "type": "function"
   },
   {
-    "type": "function",
+    "inputs": [{"internalType": "uint256","name": "_projectId","type": "uint256"}],
     "name": "depositFunds",
-    "inputs": [{"name": "_id", "type": "uint256"}],
     "outputs": [],
-    "stateMutability": "nonpayable"
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    "type": "function",
+    "inputs": [{"internalType": "uint256","name": "_projectId","type": "uint256"}],
     "name": "submitWork",
-    "inputs": [{"name": "_id", "type": "uint256"}],
     "outputs": [],
-    "stateMutability": "nonpayable"
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    "type": "function",
+    "inputs": [{"internalType": "uint256","name": "_projectId","type": "uint256"}],
     "name": "completeProject",
-    "inputs": [{"name": "_id", "type": "uint256"}],
     "outputs": [],
-    "stateMutability": "nonpayable"
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    "type": "function",
-    "name": "claimAutoRelease",
-    "inputs": [{"name": "_id", "type": "uint256"}],
-    "outputs": [],
-    "stateMutability": "nonpayable"
-  },
-  {
-    "type": "function",
-    "name": "raiseDispute",
-    "inputs": [{"name": "_id", "type": "uint256"}],
-    "outputs": [],
-    "stateMutability": "nonpayable"
-  },
-  
-  // --- 事件 (Events) ---
-  {
-    "type": "event",
-    "name": "ProjectCreated",
+    "anonymous": false,
     "inputs": [
-      {"name": "projectId", "type": "uint256", "indexed": true},
-      {"name": "buyer", "type": "address", "indexed": true},
-      {"name": "seller", "type": "address", "indexed": true},
-      {"name": "token", "type": "address", "indexed": false},
-      {"name": "amount", "type": "uint256", "indexed": false}
-    ]
+      {"indexed": true,"internalType": "bytes32","name": "projectId","type": "bytes32"},
+      {"indexed": true,"internalType": "uint256","name": "idInt","type": "uint256"},
+      {"indexed": true,"internalType": "address","name": "buyer","type": "address"},
+      {"indexed": true,"internalType": "address","name": "seller","type": "address"},
+      {"indexed": false,"internalType": "uint256","name": "amount","type": "uint256"}
+    ],
+    "name": "ProjectCreated",
+    "type": "event"
   }
 ] as const;
